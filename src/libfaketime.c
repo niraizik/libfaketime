4721ecbf22594d81c7e603464f1d70c1d5168889
Nir Aizik <niraizik@users.noreply.github.com>
2017-05-22 19:30:19 +0300

    fix sem_timedwait

--- src/libfaketime.c
+++ src/libfaketime.c
@@ -1088,7 +1088,7 @@ int sem_timedwait(sem_t *sem, const struct timespec *abs_timeout)
   {
     struct timespec tdiff, timeadj;
 
-    timespecsub(abs_timeout, &ftpl_starttime.real, &tdiff);
+    timespecsub(abs_timeout, &user_faked_time_timespec, &tdiff);
 
     if (user_rate_set)
     {
@@ -1098,7 +1098,7 @@ int sem_timedwait(sem_t *sem, const struct timespec *abs_timeout)
     {
         timeadj = tdiff;
     }
-    timespecadd(&user_faked_time_timespec, &timeadj, &real_abs_timeout);
+    timespecadd(&ftpl_starttime.real, &timeadj, &real_abs_timeout);
     real_abs_timeout_pt = &real_abs_timeout;
   }
   else
